4b231e43b4143b342f2aa71c1279776b05c55b61
Danny Wilson <danny@decube.net>
2015-06-25 15:50:58 +0200

    Clojar fork

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject fleet "0.10.2"
-  :description "Templating System for Clojure"
-  :url "http://github.com/Flamefork/fleet"
+(defproject org.clojars.touch/fleet "0.10.3"
+  :description "Templating System for Clojure, with _ fn!"
+  :url "http://github.com/touch/fleet"
   :scm {:name "git"
-        :url "https://github.com/Flamefork/fleet"}
+        :url "https://github.com/touch/fleet"}
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :signing {:gpg-key "ilia@flamefork.ru"}
 
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :global-vars {*warn-on-reflection* true}
